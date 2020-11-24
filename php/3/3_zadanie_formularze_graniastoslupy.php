0b08dbdf403fa5fd2ef904dc76f2cce1f5642d51
napieralal <70712026+napieralal@users.noreply.github.com>
2020-11-24 21:43:24 +0100

    Update 3_zadanie_formularze_graniastoslupy.php

--- php/3/3_zadanie_formularze_graniastoslupy.php
+++ php/3/3_zadanie_formularze_graniastoslupy.php
@@ -1,1 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <style>
+    td {border: 1px solid;
+      display: inline-block;
+	  text-align: center;
+	  width: 320px;
+  height: 350px;}
+    table {border: 1px solid;
+    display: inline-block;}
+    tr {text-align: center;}
+    th {text-align: center;}
+  </style>
+    <h3>Graniastosłupy:</h3>
+    <table>
+      <tr>
+      <th>Graniastosłupy</th>
+      </tr>
+      <tr>
+        <td><a href="./prostopadłościan.php"><img src="./prostopadłościan.png"></td>
+        <td><a href="./sześcian.php"><img src="./sześcian.png"></td>
+      </tr>
+      <tr>
+        <td><a href="./ostrosłup.php"><img src="./ostrosłup.png"></td>
+        <td><a href="./walec.php"><img src="./walec.png"></td>
+      </tr>
+    </table>
+  </body>
+</html>
 
