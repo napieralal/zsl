e54b44725671dc08927273380d70eee1ea4a40b6
napieralal <70712026+napieralal@users.noreply.github.com>
2020-11-27 13:54:49 +0100

    Add files via upload

---
+++ cpp/lekcja_inf_1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    string imie;
+    int wiek;
+    string login;
+    cin >> imie;
+    cin >> wiek;
+    ofstream zapis("dane_osobowe.txt", ios::app);
+    zapis << imie << " " << wiek << endl;
+    zapis.close(); 
+    string wiek1;
+    ifstream wczyt("dane_osobowe.txt");
+    if (wczyt.good()) {
+        while (wczyt >> imie >> wiek1) {
+            cout << imie << " " << wiek1 << " ";
+            login = imie + wiek1;
+            cout << login << endl;
+        }
+    }
+    else {
+        cout << "Nie uda³o sie";
+    }
+    wczyt.close();
+    return 0;
+}
