bd9ee04f8518d3786006a00709e328f638b0b7f8
napieralal <lukasz.napierala02@gmail.com>
2021-03-11 23:25:28 +0100

    init

--- js/Nowy folder/script1.js
+++
@@ -1,8 +0,0 @@
-var liczba=prompt("podaj liczbe");
-var wynik;
-if(liczba!="")
-{
-wynik=Math.abs(liczba);
-document.write(wynik+"<br>");
-}
-else document.write("Nie podałeś liczby")
--- js/Nowy folder/script2.js
+++
@@ -1,6 +0,0 @@
-var promien=prompt("Podaj wartość promienia");
-var obwód;
-var pole;
-obwód=2*Math.PI*promien;
-pole=Math.PI*Math.pow(promien, 2);
-document.write("Obwód wynosi: "+obwód+"<br> Pole wynosi: "+pole+"<br>");
